c0079fff81dfc553a6594a9a63e3d4969cbcd83f
Peter Leite <peterleite.dev@gmail.com>
2021-09-17 15:59:48 -0300

    add script para mudar o bg do header ao scrollar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const $window = $(window);
 
-
-$(document).ready(function () {
-    $('#navbar-toggler').click(function () {
+$(document).ready(()=>{
+    // header toggle
+    $('#navbar-toggler').click(()=>{
         $('.navbar-collapse').slideToggle(400);
     });
+
+    //change bg header on scrolll
+    $(window).scroll(()=>{
+        let pos = $window.scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    })
 });
