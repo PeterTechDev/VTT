d19ad87fa07e10f0033bf549105660c80b9263d7
Peter Leite <peterleite.dev@gmail.com>
2021-09-17 20:02:54 -0300

    alinhando aboutus

--- script.js
+++ script.js
@@ -15,4 +15,25 @@ $(document).ready(()=>{
             $('.navbar').removeClass('cng-navbar');
         }
     })
+
+    // owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
